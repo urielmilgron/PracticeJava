315703ff8f06487006b5d1695e2ab6765e0d6a41
Uriel Milgron <urielmilgron18@gmail.com>
2022-12-26 23:17:28 -0300

    exercise one 'sumador de números' finished

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; //Importamos el scanner de java
+public class Main {
+    public static void main(String[] args) {
+          ///////////////////////
+ /*       //Sumador de números //
+          ///////////////////////
+
+        //Declaro una variable de tipo scanner, este solo sirve para crear un elemento scanner
+        //Podemos ponerle cualquier nombre
+        Scanner userData = new Scanner(System.in);
+        //Podemos decirle al usuario que ingrese un dato imprimiendo algo por pantalla
+        System.out.print("Ingrese un número");
+        //Luego lo que ingrese el usuario lo guardamos en una variable leyendolo con el método nextInt
+        //Este solo lee números enteros
+        int numberOne = userData.nextInt();
+        System.out.print("Ingrese otro número");
+        int numberTwo = userData.nextInt();
+
+        int suma = numberOne + numberTwo;
+
+        System.out.println("La suma de estos dos números es "+suma);*/
+    }
+}
